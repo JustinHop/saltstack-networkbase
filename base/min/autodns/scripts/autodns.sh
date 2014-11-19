1bace135b1c7e8e6f44fc8585a58822addf6f785
Justin Hoppensteadt <git@justinhoppensteadt.com>
2014-11-19 10:29:02 -0800

    Added auto rackspace DNS

--- base/min/autodns/scripts/autodns.sh
+++ base/min/autodns/scripts/autodns.sh
@@ -6,3 +6,29 @@
 
 
 HOST=$(hostname)
+
+RSUSER="crowd246"
+RSID="926406"
+RSAPI="970c61c73891b471e0dd45f61051ed29"
+RSDOM="crowdrise.io"
+RSPATH=/usr/local/rsdns
+
+RESOLVED=$(dig $HOST +short)
+
+if [ $RESOLVED ]; then
+  /usr/local/rsdns/rsdns-a.sh \
+    -u $RSUSER \
+    -a $RSAPI \
+    -d $RSDOM \
+    -n $HOST \
+    -i $(hostname -I | awk '{ print $1 }') \
+    -U
+else
+  /usr/local/rsdns/rsdns-a.sh \
+    -u $RSUSER \
+    -a $RSAPI \
+    -d $RSDOM \
+    -n $HOST \
+    -i $(hostname -I | awk '{ print $1 }')
+fi
+
