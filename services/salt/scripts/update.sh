761728465dbe4684a5f30581f85c4fcd27013cb5
Justin Hoppensteadt <git@justinhoppensteadt.com>
2015-01-22 09:36:03 -0800

    Added saltupdate script

---
+++ services/salt/scripts/update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/ssh-agent /bin/bash
+
+ssh-add /srv/salt/.ssh/id_rsa
+
+cd /srv/salt
+
+for PART in filebase pillar reactor ; do 
+   cd /srv/salt/saltstack-${PART}.git
+   pwd
+   git fetch --all
+done
+
+for FORMULA in salt deployment sphinxsearch ; do
+   cd /srv/salt/${FORMULA}-formula.git
+   pwd
+   git fetch --all
+done
+
+salt-run fileserver.update
