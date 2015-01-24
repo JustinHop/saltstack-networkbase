7646cef19186e3b47b339fd2c45f2052aa939b9c
Justin Hoppensteadt <git@justinhoppensteadt.com>
2015-01-24 12:37:19 -0800

    onelesslist

--- _grains/nova.py
+++ _grains/nova.py
@@ -139,7 +139,7 @@ def cs_grains():
     try:
         for server in driver.servers.list():
             output.append(server.to_dict())
-        return({'nova': [output]})
+        return({'nova': output})
     except:
         raise
 
